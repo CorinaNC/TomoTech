00f0ddc2d418e713ee9e0494cf8e6bb04204fff9
CorinaNC <corina0615@gmail.com>
2024-11-09 13:57:41 -0600

    changed some packages.. thanks chakra for being stupid

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,12 @@
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
+import { ChakraProvider } from "@chakra-ui/react"
 import * as React from "react"
 import { createRoot } from "react-dom/client"
 import App from "./App"
+import theme from "./theme"
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider value={defaultSystem}>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
