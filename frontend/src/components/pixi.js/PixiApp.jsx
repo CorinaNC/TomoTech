ef171918a0aeeac61bc06873020134933b0ee5bd
CorinaNC <corina0615@gmail.com>
2024-11-09 21:02:29 -0600

    added pixel background

--- frontend/src/components/pixi.js/PixiApp.jsx
+++ frontend/src/components/pixi.js/PixiApp.jsx
@@ -1,38 +1,21 @@
 import { useMemo } from 'react';
 
-import { BlurFilter, TextStyle } from 'pixi.js';
-import { Stage, Container, Sprite, Text } from '@pixi/react';
+import { Application, BlurFilter } from 'pixi.js';
+import { Stage, Sprite } from '@pixi/react';
+
+import { Box } from '@chakra-ui/react'
+import Background from "../../assets/sprites/petBg.png"
+
+const textures = makeAnimatedSpriteTextures();
 
 const PixiApp = () => {
   const blurFilter = useMemo(() => new BlurFilter(2), []);
-  const bunnyUrl = 'https://pixijs.io/pixi-react/img/bunny.png';
   return (
-    <Stage width={800} height={600} options={{ background: 0x1099bb }}>
-      <Sprite image={bunnyUrl} x={300} y={150} />
-      <Sprite image={bunnyUrl} x={500} y={150} />
-      <Sprite image={bunnyUrl} x={400} y={200} />
-
-      <Container x={200} y={200}>
-        <Text
-          text="Hello World"
-          anchor={0.5}
-          x={220}
-          y={150}
-          filters={[blurFilter]}
-          style={
-            new TextStyle({
-              align: 'center',
-              fill: '0xffffff',
-              fontSize: 50,
-              letterSpacing: 20,
-              dropShadow: true,
-              dropShadowColor: '#E72264',
-              dropShadowDistance: 6,
-            })
-          }
-        />
-      </Container>
-    </Stage>
+    <Box borderColor="red" borderRadius="5px">
+        <Stage width={500} height={500}>
+        <Sprite image={Background} x={0} y={0} width={500} height={500}/>
+        </Stage>
+    </Box>
   );
 };
 
