23d79be9328c3501dee65b2dc7b02167d8bab71b
ManChat1 <mananchat03@gmail.com>
2024-11-10 00:02:20 -0600

    adding some backend and front end working

--- backend/src/event.py
+++ backend/src/event.py
@@ -1,17 +1,26 @@
 from fastapi import FastAPI
 from pydantic import BaseModel, PositiveInt
-from enum import Enum
+from typing import List, Optional
 import datetime 
 import calendar
+import tasks
 
 app = FastAPI()
 
 class Event(BaseModel):
     id : int = None
-    description : str | None = None
+    task : tasks.Task
+    description : Optional[str] = None
     start_time = datetime
     end_time = datetime
 
+events = []
+
 @app.post("/calendar/events")
-async def getCalendarEvents()
-    return 
+async def create_event(event : Event):
+    event.id = len(events) + 1
+    event.start_time = datetime.datetime.now()
+    return events.append(event)
+
+@app.get("/calendar/events", response_model = List[Event])
+async def read_events
