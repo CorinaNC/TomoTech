45f8ce29e20ff1ffc2470d4913286ce63f791189
Serena Xin <xin00051@umn.edu>
2024-11-10 03:38:33 -0600

    fixed buttons

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,38 +1,69 @@
-import { useState } from 'react';
-import { Container, Button, Link, Image, HStack } from '@chakra-ui/react'
-import FoodIcon from "../assets/jam.png"
-import SoundIcon from "../assets/sound.png"
-import MuteIcon from "../assets/mute.png"
-import MoneyIcon from "../assets/coin.png"
-import ShopIcon from  "../assets/sohp.png"
+import { useState } from "react";
+import { Container, Button, Link, Image, HStack } from "@chakra-ui/react";
+import FoodIcon from "../assets/jam.png";
+import SoundIcon from "../assets/sound.png";
+import MuteIcon from "../assets/mute.png";
+import MoneyIcon from "../assets/coin.png";
+import ShopIcon from "../assets/sohp.png";
 const Header = () => {
   const [isMuted, setIsMuted] = useState(false);
 
   const toggleSound = () => {
     setIsMuted((prevIsMuted) => !prevIsMuted);
   };
-  
-  return (
-    <Container>
-        <HStack spacing={1} mb={6} justifyContent="center">
-            <Button variant="circular" onClick={toggleSound}>
-                <Image src={isMuted ? MuteIcon : SoundIcon} width="100%" height="75%" />
-            </Button>
-            <Button variant="circular">
-                <Image src={FoodIcon} width="75%" height="75%"/>
-            </Button>
-            <Button variant="circular">
-                <Image src={ShopIcon} width="100%" height="75%" />
-            </Button>
-            <Button bg="#FFE8CB" borderRadius={100} border="5px solid #CE9964" textColor="#CE9964" _hover={{ color: "white", bg: "#CE9964" }}>
-                <Image src={MoneyIcon} width="50%" height="80%" mr={3}/>
-                <Link _hover={{ textDecoration: "none" }}>
-                    2000 Money
-                </Link>
-            </Button>
-        </HStack>
-    </Container>
-  )
-}
 
-export default Header
+  return (
+    <Container paddingX={6}>
+      <HStack spacing={6} mb={6} justifyContent="center">
+        <Button
+          variant="circular"
+          onClick={toggleSound}
+          width="75px"
+          height="75px"
+          minWidth="75px"
+          minHeight="75px"
+          borderRadius="50%"
+          flexShrink={0}
+        >
+          <Image src={isMuted ? MuteIcon : SoundIcon} />
+        </Button>
+        <Button
+          variant="circular"
+          width="75px"
+          height="75px"
+          minWidth="75px"
+          minHeight="75px"
+          borderRadius="50%"
+          flexShrink={0}
+        >
+          <Image src={FoodIcon} />
+        </Button>
+        <Button
+          variant="circular"
+          width="75px"
+          height="75px"
+          minWidth="75px"
+          minHeight="75px"
+          borderRadius="50%"
+          flexShrink={0}
+        >
+          <Image src={ShopIcon} />
+        </Button>
+        <Button
+          bg="#FFE8CB"
+          height="75px"
+          borderRadius={100}
+          border="5px solid #CE9964"
+          textColor="#CE9964"
+          _hover={{ color: "white", bg: "#CE9964" }}
+          flexShrink={0}
+        >
+          <Image src={MoneyIcon} width="50%" height="80%" mr={3} />
+          <Link _hover={{ textDecoration: "none" }}>2000 Money</Link>
+        </Button>
+      </HStack>
+    </Container>
+  );
+};
+
+export default Header;
